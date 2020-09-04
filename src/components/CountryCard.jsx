bd480dde3ba2a39ae791edda64c1c27e1936387b
Erik Björn <erik.g.bjoern@gmail.com>
2020-09-04 12:03:45 +0200

    adding full names for continents and refactoring keywords

--- src/components/CountryCard.jsx
+++ src/components/CountryCard.jsx
@@ -1,32 +1,38 @@
 import React from "react"
 import styles from "../stylesheets/CountryCard.module.css"
+import { continents } from "../modules/continents"
 
 const CountryCard = ({ country }) => {
-  let languagesArray = []
+  if (!country) return
 
-  country &&
-    Object.entries(country.languages).forEach(([k, v]) => {
-      languagesArray.push(v.name)
-    })
+  const languagesArr = Object.entries(country.languages).map(([k, v]) => v.name)
 
-  const capital = country?.capital
-  const continent = country?.continent.code
-  const languages = languagesArray.join(", ")
-  const currency = country?.currency?.split(",").join(", ")
+  const capital = country.capital
+  const continent = continents[country.continent.code]
+  const languages = languagesArr.join(", ")
+  const currency = country.currency?.split(",").join(", ")
 
   const keyWordsArr = [capital, continent, languages, currency]
-  const keyWords = keyWordsArr.filter((w) => w != null && w !== "").join(" ● ")
+
+  const keyWords = keyWordsArr
+    .filter((w) => w != null && w !== "")
+    .map((w, i) => {
+      return i === 0 ? (
+        <span>{w}</span>
+      ) : (
+        <>
+          <span className={styles.bullet}> ● </span>
+          <span>{w}</span>
+        </>
+      )
+    })
 
   return (
     <div className={styles.container}>
-      <span className={styles.flag}>{country?.emoji}</span>
+      <span className={styles.flag}>{country.emoji}</span>
       <div className={styles.textContainer}>
-        <h3 className={styles.heading}>
-          {country?.name}
-        </h3>
-        <p className={styles.keyWords}>
-          {keyWords}
-        </p>
+        <h3 className={styles.heading}>{country.name}</h3>
+        <p className={styles.keyWords}>{keyWords}</p>
       </div>
     </div>
   )
