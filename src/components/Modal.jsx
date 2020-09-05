d2ccc6063de020ac2bb70ba4afe05d031c07d925
Erik Björn <erik.g.bjoern@gmail.com>
2020-09-05 14:54:09 +0200

    responsive to small viewports, fixing overlay z-index by removing .container

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,31 +4,46 @@ import FlagIcon from "../modules/flagIcon"
 import { continents } from "../modules/continents"
 import CountryCard from "./CountryCard"
 
-const Modal = ({ country, rndmCountriesInCont, isOpen, setModalIsOpen, total, setActiveCountry }) => {
-  debugger
+const Modal = ({
+  country,
+  rndmCountriesInCont,
+  isOpen,
+  setModalIsOpen,
+  total,
+  setActiveCountry,
+}) => {
 
   if (!isOpen) return null
 
   const otherCountries = rndmCountriesInCont
     .slice(0, 3)
-    .map((country) => <CountryCard key={country.code} country={country} setModalIsOpen={setModalIsOpen} setActiveCountry={setActiveCountry} />)
+    .map((country) => (
+      <CountryCard
+        key={country.code}
+        country={country}
+        setModalIsOpen={setModalIsOpen}
+        setActiveCountry={setActiveCountry}
+      />
+    ))
 
   return (
     <>
       <div className={styles.overlay} onClick={() => setModalIsOpen(false)} />
-      <div className={styles.container}>
-        <div className={styles.modal}>
+      <div className={styles.modal}>
+        <div className={styles.topContainer}>
           <div className={styles.flag}>
             <FlagIcon code={country.code.toLowerCase()} size={"5x"} />
           </div>
-          <h3 className={styles.heading}>{country.name}</h3>
-          <p className={styles.continent}>{continents[country.continent.code]}</p>
-          <p className={styles.otherCountriesText}>
-            Other countries in {continents[country.continent.code]}
-          </p>
-          <div className={styles.cardContainer}>{otherCountries}</div>
-          <p>+ {total - 3} more</p>
+          <div className={styles.textContainer}>
+            <h3 className={styles.heading}>{country.name}</h3>
+            <p className={styles.continent}>{continents[country.continent.code]}</p>
+          </div>
         </div>
+        <p className={styles.otherCountriesText}>
+          Other countries in {continents[country.continent.code]}
+        </p>
+        <div className={styles.cardContainer}>{otherCountries}</div>
+        <p>+ {total - 3} more</p>
       </div>
     </>
   )
