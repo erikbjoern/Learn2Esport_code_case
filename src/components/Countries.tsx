1720bf5b3fca935889cf46c44a9be96f18cc083c
Erik Björn <erik.g.bjoern@gmail.com>
2020-09-07 18:10:23 +0200

    refactoring apollo query using useQuery

--- src/components/Countries.tsx
+++ src/components/Countries.tsx
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react"
 import CountryCard from "./CountryCard"
 import Modal from "./Modal"
-import { fetchCountries } from "../modules/fetchCountries"
 import { findMatchInBeginning, findMatchElsewhere } from "../helpers/sortResult"
 import styles from "../stylesheets/Countries.module.css"
 import { Country } from "../types"
+import { gql, useQuery } from "@apollo/client"
+
+interface Countries {
+  countries: Country[]
+}
 
 interface Props {
-  filter: string;
+  filter: string
 }
 
 const Countries: React.FC<Props> = ({ filter }): JSX.Element => {
@@ -16,27 +20,64 @@ const Countries: React.FC<Props> = ({ filter }): JSX.Element => {
   const [activeCountry, setActiveCountry] = useState<Country | null>(null)
   const [modalIsOpen, setModalIsOpen] = useState(false)
   const rndmCountriesInCont: Country[] = []
-  const inSameCont = countryList.filter((c: Country) => c.continent.code === activeCountry?.continent?.code)
+  const inSameCont: Country[] = countryList
+    ? countryList.filter((c: Country) => c.continent.code === activeCountry?.continent?.code)
+    : []
   const inSameContTotal: number = inSameCont.length
+
+  const COUNTRIES = gql`
+    {
+      countries {
+        capital
+        code
+        continent {
+          code
+        }
+        currency
+        languages {
+          name
+        }
+        name
+      }
+    }
+  `
+
+  const { loading, error, data } = useQuery<Countries>(COUNTRIES)
+
+  useEffect(() => {
+    if (!loading && !error && data) {
+      setCountryList(data.countries)
+    }
+  }, [loading, error, data])
+
+  useEffect(() => {
+    document.addEventListener("keydown", escape)
+
+    return () => document.removeEventListener("keydown", escape)
+  }, [])
+
+  useEffect(() => {
+    if (countryList) {
+      const filteredAndSorted: Country[] = [
+        ...findMatchInBeginning(countryList, filter),
+        ...findMatchElsewhere(countryList, filter),
+      ]
+      setFilteredList(filteredAndSorted)
+    }
+  }, [filter, countryList])
+
+  const escape = (e: KeyboardEvent) => {
+    if (e.keyCode === 27) {
+      setModalIsOpen(false)
+    }
+  }
 
   for (let i = 0; i < 3; i++) {
     const randomCountry = inSameCont[Math.floor(Math.random() * inSameCont.length)]
     if (!rndmCountriesInCont.find((c) => c === randomCountry) && randomCountry !== activeCountry) {
       rndmCountriesInCont.push(randomCountry)
     } else {
-      i-=1
-    }
-  }
-
-  const fetch = async () => {
-    let response: any;
-    response = await fetchCountries()
-    setCountryList(response.data.countries)
-  }
-
-  const escape = (e: KeyboardEvent) => {
-    if (e.keyCode === 27) {
-      setModalIsOpen(false)
+      i -= 1
     }
   }
 
@@ -50,21 +91,6 @@ const Countries: React.FC<Props> = ({ filter }): JSX.Element => {
       fromModal={false}
     />
   ))
-
-  useEffect(() => {
-    fetch()
-    document.addEventListener("keydown", escape)
-
-    return () => document.removeEventListener("keydown", escape)
-  }, [])
-
-  useEffect(() => {
-    const filteredAndSorted: Country[] = [
-      ...findMatchInBeginning(countryList, filter),
-      ...findMatchElsewhere(countryList, filter),
-    ]
-    setFilteredList(filteredAndSorted)
-  }, [filter, countryList])
 
   return (
     <>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  uri: "https://countries.trevorblades.com",
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  document.getElementById("root")
+)
