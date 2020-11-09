ecd3b544c2385ec9db3852ce4c37eef8da88da4f
Erik Björn <erik.g.bjoern@gmail.com>
2020-11-09 13:11:44 +0100

    refactoring randomCountriesInContinent to simplify checking for duplicates

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -20,11 +20,6 @@ const Modal: React.FC<Props> = ({
 }): JSX.Element => {
   const [closing, setClosing] = useState(false)
   const continentName = continentNames[activeCountry.continent.code]
-  const inSameContinent: Country[] = countryList
-    ? countryList.filter((c: Country) => c.continent.code === activeCountry?.continent?.code)
-    : []
-  const randomCountriesInContinent: Country[] = []
-  const inSameContinentTotal: number = inSameContinent.length
 
   const scrollIfTooSmall = () => {
     window.innerHeight < 700 && window.scrollTo({ top: 100, behavior: "smooth" })
@@ -39,7 +34,7 @@ const Modal: React.FC<Props> = ({
   useEffect(() => {
     window.addEventListener("resize", scrollIfTooSmall)
     document.addEventListener("keydown", closeOnEscape)
-    
+
     return () => {
       window.removeEventListener("resize", scrollIfTooSmall)
       document.removeEventListener("keydown", closeOnEscape)
@@ -57,17 +52,20 @@ const Modal: React.FC<Props> = ({
       setFilter(continentName)
     }, 700)
   }
-  
-  for (let i = 0; i < 3; i++) {
-    const randomCountry = inSameContinent[Math.floor(Math.random() * inSameContinent.length)]
-    if (!randomCountriesInContinent.find((c) => c === randomCountry) && randomCountry !== activeCountry) {
-      randomCountriesInContinent.push(randomCountry)
-    } else {
-      i -= 1
+
+  const inSameContinent = countryList.filter((c: Country) => c.continent.code === activeCountry.continent.code)
+  const randomCountriesInContinent: Set<Country> = new Set()
+  const inSameContinentTotal: number = inSameContinent.length
+
+  while (randomCountriesInContinent.size < 3) {
+    const randomCountry: Country =
+      inSameContinent[Math.floor(Math.random() * inSameContinent.length)]
+    if (randomCountry !== activeCountry) {
+      randomCountriesInContinent.add(randomCountry)
     }
   }
 
-  const otherCountries = randomCountriesInContinent.map((country: Country) => (
+  const otherCountries = Array.from(randomCountriesInContinent).map((country: Country) => (
     <CountryCard
       key={country.code}
       country={country}
