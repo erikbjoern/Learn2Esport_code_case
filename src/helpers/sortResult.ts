a49362d8ff29b96e0b8695a63a027ae265178a45
Erik Björn <erik.g.bjoern@gmail.com>
2020-11-10 12:53:25 +0100

    adding regex to match countries in sortResult

--- src/helpers/sortResult.ts
+++ src/helpers/sortResult.ts
@@ -1,28 +1,26 @@
 import { Country } from "../types"
 
 export const findMatchInBeginning = (array: Country[], filter: string) => {
-  filter = filter.toLowerCase()
+  const startsWithSearchText = new RegExp("^" + filter.toLowerCase())
 
   return array
     .filter(
       (country) =>
-        country.name.toLowerCase().slice(0, filter.length) === filter ||
-        country.native.toLowerCase().slice(0, filter.length) === filter
+        country.name.toLowerCase().match(startsWithSearchText) ||
+        country.native.toLowerCase().match(startsWithSearchText)
     )
     .sort((a, b) => a["name"].localeCompare(b["name"]))
 }
 
 export const findMatchElsewhere = (array: Country[], filter: string) => {
-  filter = filter.toLowerCase()
+  const containsSearchTextButNotInBeginning = new RegExp("(?<!^)" + filter.toLowerCase())
 
   return filter.length < 3
     ? []
     : array
         .filter(
           (country) =>
-            country.name.toLowerCase().slice(0, filter.length) !== filter &&
-            country.native.toLowerCase().slice(0, filter.length) !== filter &&
-            country.name.toLowerCase().includes(filter)
+            country.name.toLowerCase().match(containsSearchTextButNotInBeginning)
         )
         .sort((a, b) => a["name"].localeCompare(b["name"]))
 }
